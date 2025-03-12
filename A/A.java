fe631331774971ed8737e10f53dcb36d9b822048
taofy <1605233432>
2025-03-12 14:25:56 +0800

    提交方法一

--- A/A.java
+++ A/A.java
@@ -4,4 +4,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("第一次提交");
     }
+    public void methodsFirst(){
+        System.out.println("第二次提交");
+    }
 }
